f8970a56277e5d68ddea8dfd24bd49fe7fd765a5
meiheW <1925257530@qq.com>
2020-12-13 02:00:27 +0800

    init leetcode plugin start work

---
+++ src/main/java/com/tomster/leetcode/LeetcodePracticeApplication.java
@@ -0,0 +1,17 @@
+package com.tomster.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author meihewang
+ * @date 2020/12/13  1:47
+ */
+@SpringBootApplication
+public class LeetcodePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodePracticeApplication.class, args);
+    }
+
+}
