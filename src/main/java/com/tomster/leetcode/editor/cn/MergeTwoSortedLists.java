71d8a7d393444fc00bdab5b62ddf4b0c98fc396f
meiheW <1925257530@qq.com>
2020-12-19 22:47:00 +0800

    21.23 合并2或者K个有序链表 优先队列

---
+++ src/main/java/com/tomster/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,61 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表 
+// 👍 1443 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.ListNode;
+
+/**
+ * @author: tomster
+ * @data: 2020-12-19 16:50:21
+ */
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+            ListNode pilot = new ListNode(1);
+            ListNode cur = pilot;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+            }
+            cur.next = l1 != null ? l1 : l2;
+
+            return pilot.next;
+
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
