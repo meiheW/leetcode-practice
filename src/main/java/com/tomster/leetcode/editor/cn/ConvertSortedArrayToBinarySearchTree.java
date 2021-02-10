876eadfc3829cf575a4c9c7c2f77b80037698e9e
meiheW <1925257530@qq.com>
2021-02-11 01:50:47 +0800

    108.109 有序数组/链表转换为二叉搜索树 递归

---
+++ src/main/java/com/tomster/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,71 @@
+//将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。 
+//
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。 
+//
+// 示例: 
+//
+// 给定有序数组: [-10,-3,0,5,9],
+//
+//一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 695 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.TreeNode;
+
+/**
+ * @author: tomster
+ * @data: 2021-02-11 01:01:45
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new ConvertSortedArrayToBinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return null;
+            }
+            return sortedArrayToBST(nums, 0, nums.length - 1);
+
+        }
+
+        private TreeNode sortedArrayToBST(int[] nums, int begin, int end) {
+            if (begin > end) {
+                return null;
+            }
+            int middle = (begin + end) / 2;
+            TreeNode node = new TreeNode(nums[middle]);
+            node.left = sortedArrayToBST(nums, begin, middle - 1);
+            node.right = sortedArrayToBST(nums, middle + 1, end);
+            return node;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
