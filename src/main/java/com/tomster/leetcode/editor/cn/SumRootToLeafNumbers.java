fa04da661644c5ef31320bda42426b33e6bbbebd
meiheW <1925257530@qq.com>
2021-02-12 23:58:40 +0800

    129. 求根到叶子节点数字之和

---
+++ src/main/java/com/tomster/leetcode/editor/cn/SumRootToLeafNumbers.java
@@ -0,0 +1,89 @@
+//给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+//
+// 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+//
+// 计算从根到叶子节点生成的所有数字之和。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例 1:
+//
+// 输入: [1,2,3]
+//    1
+//   / \
+//  2   3
+//输出: 25
+//解释:
+//从根到叶子节点路径 1->2 代表数字 12.
+//从根到叶子节点路径 1->3 代表数字 13.
+//因此，数字总和 = 12 + 13 = 25.
+//
+// 示例 2:
+//
+// 输入: [4,9,0,5,1]
+//    4
+//   / \
+//  9   0
+// / \
+//5   1
+//输出: 1026
+//解释:
+//从根到叶子节点路径 4->9->5 代表数字 495.
+//从根到叶子节点路径 4->9->1 代表数字 491.
+//从根到叶子节点路径 4->0 代表数字 40.
+//因此，数字总和 = 495 + 491 + 40 = 1026.
+// Related Topics 树 深度优先搜索
+// 👍 309 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.TreeNode;
+
+/**
+ * @author: tomster
+ * @data: 2021-02-11 01:53:40
+ */
+public class SumRootToLeafNumbers {
+    public static void main(String[] args) {
+        Solution solution = new SumRootToLeafNumbers().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode root, int pre) {
+            if (root == null) {
+                return 0;
+            }
+            int sum = pre * 10 + root.val;
+            //节点为叶子节点
+            if (root.left == null && root.right == null) {
+                return sum;
+            } else {
+                return dfs(root.left, sum) + dfs(root.right, sum);
+            }
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
