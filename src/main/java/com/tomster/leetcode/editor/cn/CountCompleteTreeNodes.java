9ec06d2df6e730b3d63f2bfd93ed568631da278c
meiheW <1925257530@qq.com>
2021-02-13 21:33:06 +0800

    222. 完全二叉树的节点个数

---
+++ src/main/java/com/tomster/leetcode/editor/cn/CountCompleteTreeNodes.java
@@ -0,0 +1,107 @@
+//给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。 
+//
+// 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层
+//为第 h 层，则该层包含 1~ 2h 个节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3,4,5,6]
+//输出：6
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：0
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目范围是[0, 5 * 104] 
+// 0 <= Node.val <= 5 * 104 
+// 题目数据保证输入的树是 完全二叉树 
+// 
+//
+// 
+//
+// 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？ 
+// Related Topics 树 二分查找 
+// 👍 433 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.TreeNode;
+
+/**
+ * @author: tomster
+ * @data: 2021-02-13 21:02:59
+ */
+public class CountCompleteTreeNodes {
+    public static void main(String[] args) {
+        Solution solution = new CountCompleteTreeNodes().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        /**
+         * 参考一般二叉树和满二叉树的节点数计算方法
+         *
+         * @param root
+         * @return
+         */
+        public int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            TreeNode left = root;
+            TreeNode right = root;
+            int hl = 0, hr = 0;
+            while (left != null) {
+                left = left.left;
+                hl++;
+            }
+            while (right != null) {
+                right = right.right;
+                hr++;
+            }
+
+            if (hl == hr) {
+                //满二叉树
+                return (int) Math.pow(2, hl) - 1;
+            }
+            //一般二叉树
+            return countNodes(root.left) + countNodes(root.right) + 1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
