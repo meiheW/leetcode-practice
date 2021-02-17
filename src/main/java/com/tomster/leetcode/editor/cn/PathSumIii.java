845b7d02d2ba183744c9d33fd239d1433a857ccc
meiheW <1925257530@qq.com>
2021-02-17 12:23:01 +0800

    437. 路径总和 III

---
+++ src/main/java/com/tomster/leetcode/editor/cn/PathSumIii.java
@@ -0,0 +1,87 @@
+//给定一个二叉树，它的每个结点都存放着一个整数值。 
+//
+// 找出路径和等于给定数值的路径总数。 
+//
+// 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。 
+//
+// 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。 
+//
+// 示例： 
+//
+// root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+//
+//      10
+//     /  \
+//    5   -3
+//   / \    \
+//  3   2   11
+// / \   \
+//3  -2   1
+//
+//返回 3。和等于 8 的路径有:
+//
+//1.  5 -> 3
+//2.  5 -> 2 -> 1
+//3.  -3 -> 11
+// 
+// Related Topics 树 
+// 👍 740 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.TreeNode;
+
+/**
+ * @author: tomster
+ * @data: 2021-02-17 12:03:34
+ */
+public class PathSumIii {
+    public static void main(String[] args) {
+        Solution solution = new PathSumIii().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int pathSum(TreeNode root, int sum) {
+            if (root == null) {
+                return 0;
+            }
+            traversal(root, sum);
+            pathSum(root.left, sum);
+            pathSum(root.right, sum);
+            return num;
+        }
+
+        int num;
+
+        private void traversal(TreeNode root, int sum) {
+            if (root == null) {
+                return;
+            }
+            if (root.val == sum) {
+                num++;
+            }
+            traversal(root.left, sum - root.val);
+            traversal(root.right, sum - root.val);
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
