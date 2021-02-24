6b53f47e1d536c16520ce4600ef6671d73ffb3ad
meiheW <1925257530@qq.com>
2021-02-24 21:56:28 +0800

    783. 二叉搜索树节点最小距离

---
+++ src/main/java/com/tomster/leetcode/editor/cn/MinimumDistanceBetweenBstNodes.java
@@ -0,0 +1,89 @@
+//给定一个二叉搜索树的根节点 root，返回树中任意两节点的差的最小值。 
+//
+// 
+//
+// 示例： 
+//
+// 输入: root = [4,2,6,1,3,null,null]
+//输出: 1
+//解释:
+//注意，root是树节点对象(TreeNode object)，而不是数组。
+//
+//给定的树 [4,2,6,1,3,null,null] 可表示为下图:
+//
+//          4
+//        /   \
+//      2      6
+//     / \    
+//    1   3  
+//
+//最小的差值是 1, 它是节点1和节点2的差值, 也是节点3和节点2的差值。 
+//
+// 
+//
+// 注意： 
+//
+// 
+// 二叉树的大小范围在 2 到 100。 
+// 二叉树总是有效的，每个节点的值都是整数，且不重复。 
+// 本题与 530：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/ 
+//相同 
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 112 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import com.tomster.leetcode.entity.TreeNode;
+
+/**
+ * @author: tomster
+ * @data: 2021-02-24 21:51:36
+ */
+public class MinimumDistanceBetweenBstNodes {
+    public static void main(String[] args) {
+        Solution solution = new MinimumDistanceBetweenBstNodes().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int minDiffInBST(TreeNode root) {
+            inOrderTraversal(root);
+            return min;
+        }
+
+        TreeNode pre = null;
+        int min = Integer.MAX_VALUE;
+
+        private void inOrderTraversal(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            inOrderTraversal(root.left);
+            if (pre == null) {
+                pre = root;
+            } else {
+                min = Math.min(min, root.val - pre.val);
+                pre = root;
+            }
+            inOrderTraversal(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
