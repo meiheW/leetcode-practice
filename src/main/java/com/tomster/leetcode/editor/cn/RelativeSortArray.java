ae2e7894dde56083b63435ebd9396340eea8c6b5
meiheW <1925257530@qq.com>
2021-05-15 15:59:28 +0800

    1122. 数组的相对排序

---
+++ src/main/java/com/tomster/leetcode/editor/cn/RelativeSortArray.java
@@ -0,0 +1,81 @@
+//给你两个数组，arr1 和 arr2， 
+//
+// 
+// arr2 中的元素各不相同 
+// arr2 中的每个元素都出现在 arr1 中 
+// 
+//
+// 对 arr1 中的元素进行排序，使 arr1 中项的相对顺序和 arr2 中的相对顺序相同。未在 arr2 中出现过的元素需要按照升序放在 arr1 的末
+//尾。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+//输出：[2,2,2,1,4,3,3,9,6,7,19]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr1.length, arr2.length <= 1000 
+// 0 <= arr1[i], arr2[i] <= 1000 
+// arr2 中的元素 arr2[i] 各不相同 
+// arr2 中的每个元素 arr2[i] 都出现在 arr1 中 
+// 
+// Related Topics 排序 数组 
+// 👍 174 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: tomster
+ * @data: 2021-05-15 15:39:32
+ */
+public class RelativeSortArray {
+    public static void main(String[] args) {
+        Solution solution = new RelativeSortArray().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] relativeSortArray(int[] arr1, int[] arr2) {
+            int[] result = new int[arr1.length];
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int item : arr1) {
+                map.put(item, map.getOrDefault(item, 0) + 1);
+            }
+            int index = 0;
+            //添加arr2中的元素
+            for (int value : arr2) {
+                int nums = map.get(value);
+                while (nums-- > 0) {
+                    result[index++] = value;
+                }
+                map.remove(value);
+            }
+            ArrayList<Integer> list = new ArrayList<>(map.keySet());
+            Collections.sort(list);
+            //添加不在arr2中的元素
+            for (int value : list) {
+                int nums = map.get(value);
+                while (nums-- > 0) {
+                    result[index++] = value;
+                }
+                map.remove(value);
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
