e9045857e847f8f96637570954156208289255b9
meiheW <1925257530@qq.com>
2021-07-13 23:51:17 +0800

    387. 字符串中的第一个唯一字符

---
+++ src/main/java/com/tomster/leetcode/editor/cn/FirstUniqueCharacterInAString.java
@@ -0,0 +1,54 @@
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 队列 哈希表 字符串 计数 
+// 👍 414 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author: tomster
+ * @data: 2021-07-13 22:50:44
+ */
+public class FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+        Solution solution = new FirstUniqueCharacterInAString().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int firstUniqChar(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (map.get(s.charAt(i)) == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
