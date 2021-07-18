7b7b5da5e36747ed9c74889445c9406a70896884
meiheW <1925257530@qq.com>
2021-07-18 21:59:28 +0800

    239. 滑动窗口最大值

--- src/main/java/com/tomster/leetcode/editor/cn/SlidingWindowMaximum.java
+++ src/main/java/com/tomster/leetcode/editor/cn/SlidingWindowMaximum.java
@@ -62,9 +62,7 @@
 
 package com.tomster.leetcode.editor.cn;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * @author: tomster
@@ -81,7 +79,60 @@ public class SlidingWindowMaximum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
+        //4.双向队列
         public int[] maxSlidingWindow(int[] nums, int k) {
+            Deque<Integer> deque = new LinkedList<>();
+            for (int i = 0; i < k; i++) {
+                while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+            }
+            int n = nums.length;
+            int[] result = new int[n - k + 1];
+            result[0] = nums[deque.peekFirst()];
+            for (int i = k; i < n; i++) {
+                //单调递减队列
+                while (!deque.isEmpty() && nums[i] > nums[deque.peekLast()]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+                while (deque.peekFirst() <= i - k) {
+                    deque.pollFirst();
+                }
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+            return result;
+        }
+
+
+        //3.堆
+        public int[] maxSlidingWindow3(int[] nums, int k) {
+            int n = nums.length;
+            PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] pair1, int[] pair2) {
+                    return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair1[1] - pair2[1];
+                }
+            });
+            for (int i = 0; i < k; i++) {
+                heap.offer(new int[]{nums[i], i});
+            }
+            int[] result = new int[n - k + 1];
+            result[0] = heap.peek()[0];
+            for (int i = k; i < n; i++) {
+                heap.offer(new int[]{nums[i], i});
+                while (heap.peek()[1] <= i - k) {
+                    heap.poll();
+                }
+                result[i - k + 1] = heap.peek()[0];
+
+            }
+            return result;
+        }
+
+        //2.treeMap效率有点低
+        public int[] maxSlidingWindow2(int[] nums, int k) {
             int n = nums.length;
             int[] result = new int[n - k + 1];
             TreeMap<Integer, Integer> treeMap = new TreeMap<>();
