9adb83921e84c2da6b4afa1de39ad9144f524f50
meiheW <1925257530@qq.com>
2021-08-14 21:43:15 +0800

    139. 单词拆分

---
+++ src/main/java/com/tomster/leetcode/editor/cn/WordBreak.java
@@ -0,0 +1,68 @@
+//给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。 
+//
+// 说明： 
+//
+// 
+// 拆分时可以重复使用字典中的单词。 
+// 你可以假设字典中没有重复的单词。 
+// 
+//
+// 示例 1： 
+//
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+//输出: true
+//解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+// 
+//
+// 示例 2： 
+//
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+//输出: true
+//解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+//     注意你可以重复使用字典中的单词。
+// 
+//
+// 示例 3： 
+//
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+//输出: false
+// 
+// Related Topics 字典树 记忆化搜索 哈希表 字符串 动态规划 
+// 👍 1114 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * @author: tomster
+ * @data: 2021-08-14 21:30:27
+ */
+public class WordBreak {
+    public static void main(String[] args) {
+        Solution solution = new WordBreak().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //输入: s = "applepenapple", wordDict = ["apple", "pen"]
+        //输出: true
+        public boolean wordBreak(String s, List<String> wordDict) {
+            int n = s.length();
+            boolean[] dp = new boolean[n + 1];
+            dp[0] = true;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[n];
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
