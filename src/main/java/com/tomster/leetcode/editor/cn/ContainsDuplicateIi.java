6680b4169f1581b16b5676915863c1734165dfc6
meiheW <1925257530@qq.com>
2021-08-22 14:48:19 +0800

    219. 存在重复元素 II

---
+++ src/main/java/com/tomster/leetcode/editor/cn/ContainsDuplicateIi.java
@@ -0,0 +1,55 @@
+//给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值
+// 至多为 k。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,2,3,1], k = 3
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: nums = [1,0,1,1], k = 1
+//输出: true 
+//
+// 示例 3: 
+//
+// 输入: nums = [1,2,3,1,2,3], k = 2
+//输出: false 
+// Related Topics 数组 哈希表 滑动窗口 
+// 👍 298 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: tomster
+ * @data: 2021-08-22 14:42:38
+ */
+public class ContainsDuplicateIi {
+    public static void main(String[] args) {
+        Solution solution = new ContainsDuplicateIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //输入: nums = [1,2,3,1], k = 3
+        //输出: true
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int cur = nums[i];
+                if (map.containsKey(cur) && i - map.get(cur) <= k) {
+                    return true;
+                }
+                map.put(cur, i);
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
