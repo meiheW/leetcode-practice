fc5e9c3a4a50c5bb01587e2d99cb8b3d5258e76d
meiheW <1925257530@qq.com>
2021-09-04 23:58:18 +0800

    690. 员工的重要性

---
+++ src/main/java/com/tomster/leetcode/entity/bo/Employee.java
@@ -0,0 +1,13 @@
+package com.tomster.leetcode.entity.bo;
+
+import java.util.List;
+
+/**
+ * @author meihewang
+ * @date 2021/09/04  23:56
+ */
+public class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+}
