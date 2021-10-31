a75319c9e54187abdccc7a4adb9055e2175eb722
meiheW <1925257530@qq.com>
2021-10-31 19:46:23 +0800

    500. 键盘行

--- src/main/java/com/tomster/leetcode/editor/cn/KeyboardRow.java
+++ src/main/java/com/tomster/leetcode/editor/cn/KeyboardRow.java
@@ -63,8 +63,37 @@ public class KeyboardRow {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public String[] findWords(String[] words) {
 
+        public String[] findWords(String[] words) {
+            String[] rows = new String[]{"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+            int[] rowArr = new int[26];
+            for (int i = 0; i < 3; i++) {
+                String row = rows[i];
+                for (int j = 0; j < row.length(); j++) {
+                    rowArr[row.charAt(j) - 'a'] = i;
+                }
+            }
+            List<String> ans = new ArrayList<>();
+            for (String word : words) {
+                int index = rowArr[Character.toLowerCase(word.charAt(0)) - 'a'];
+                boolean sameRow = true;
+                for (int i = 1; i < word.length(); i++) {
+                    if (rowArr[Character.toLowerCase(word.charAt(i)) - 'a'] != index) {
+                        sameRow = false;
+                        break;
+                    }
+                }
+                if (sameRow) {
+                    ans.add(word);
+                }
+            }
+            String[] result = new String[ans.size()];
+
+            ans.toArray(result);
+            return result;
+        }
+
+        public String[] findWords1(String[] words) {
             Map<Character, Integer> map = new HashMap<>();
             String[] rows = new String[]{"qwertyuiop", "asdfghjkl", "zxcvbnm"};
             for (int i = 0; i < rows.length; i++) {
