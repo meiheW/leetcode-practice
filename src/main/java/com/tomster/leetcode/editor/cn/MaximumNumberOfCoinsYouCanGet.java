b32fd780f247e38a806d0056aa1bc9d407f51abb
meiheW <1925257530@qq.com>
2021-11-06 21:36:43 +0800

    1561. 你可以获得的最大硬币数目

--- src/main/java/com/tomster/leetcode/editor/cn/MaximumNumberOfCoinsYouCanGet.java
+++ src/main/java/com/tomster/leetcode/editor/cn/MaximumNumberOfCoinsYouCanGet.java
@@ -64,7 +64,19 @@ public class MaximumNumberOfCoinsYouCanGet {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public int maxCoins(int[] piles) {
+            Arrays.sort(piles);
+            int n = piles.length;
+            int ans = 0;
+            for (int i = n / 3; i < n; i += 2) {
+                ans += piles[i];
+            }
+            return ans;
+        }
+
+
+        public int maxCoins1(int[] piles) {
             Arrays.sort(piles);
             LinkedList<Integer> list = new LinkedList<>();
             for (int pile : piles) {
