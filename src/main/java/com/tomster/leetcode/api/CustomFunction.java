26cbb8111f22b114f0f8bee7180787b434a8da08
meiheW <1925257530@qq.com>
2021-11-21 11:13:36 +0800

    1237. 找出给定方程的正整数解

---
+++ src/main/java/com/tomster/leetcode/api/CustomFunction.java
@@ -0,0 +1,11 @@
+package com.tomster.leetcode.api;
+
+/**
+ * @author meihewang
+ * @date 2021/11/21  11:05
+ */
+public class CustomFunction {
+    public int f(int x, int y) {
+        return x + y;
+    }
+}
