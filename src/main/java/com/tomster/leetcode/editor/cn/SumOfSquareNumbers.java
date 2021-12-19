a33b08d3e6ef38ce4810134a9ad4b979c6df8228
meiheW <1925257530@qq.com>
2021-12-19 15:16:34 +0800

    633. 平方数之和

---
+++ src/main/java/com/tomster/leetcode/editor/cn/SumOfSquareNumbers.java
@@ -0,0 +1,76 @@
+//给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：c = 5
+//输出：true
+//解释：1 * 1 + 2 * 2 = 5
+// 
+//
+// 示例 2： 
+//
+// 输入：c = 3
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：c = 4
+//输出：true
+// 
+//
+// 示例 4： 
+//
+// 输入：c = 2
+//输出：true
+// 
+//
+// 示例 5： 
+//
+// 输入：c = 1
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= c <= 231 - 1 
+// 
+// Related Topics 数学 双指针 二分查找 
+// 👍 323 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+/**
+ * @author: tomster
+ * @data: 2021-12-19 14:21:10
+ */
+public class SumOfSquareNumbers {
+    public static void main(String[] args) {
+        Solution solution = new SumOfSquareNumbers().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean judgeSquareSum(int c) {
+            int low = 1;
+            int high = (int) Math.sqrt(c + 0.05);
+            while (low < high) {
+                int sum = low * low + high * high;
+                if (sum > c) {
+                    high--;
+                } else if (sum < c) {
+                    low++;
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
