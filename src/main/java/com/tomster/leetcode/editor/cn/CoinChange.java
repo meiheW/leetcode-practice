83aab9441d10f2d3ba87dd2f2c4e2634341ed411
meiheW <1925257530@qq.com>
2022-03-17 16:21:44 +0800

    322. 零钱兑换

---
+++ src/main/java/com/tomster/leetcode/editor/cn/CoinChange.java
@@ -0,0 +1,74 @@
+//给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。 
+//
+// 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。 
+//
+// 你可以认为每种硬币的数量是无限的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：coins = [1, 2, 5], amount = 11
+//输出：3 
+//解释：11 = 5 + 5 + 1 
+//
+// 示例 2： 
+//
+// 
+//输入：coins = [2], amount = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：coins = [1], amount = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 231 - 1 
+// 0 <= amount <= 104 
+// 
+// Related Topics 广度优先搜索 数组 动态规划 
+// 👍 1796 👎 0
+
+package com.tomster.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * @author: tomster
+ * @data: 2022-03-17 15:30:41
+ */
+public class CoinChange {
+    public static void main(String[] args) {
+        Solution solution = new CoinChange().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            int max = amount + 1;
+            //dp[i]表示构成金额总和为i的硬币的最小数量
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, max);
+            dp[0] = 0;
+            for (int i = 1; i < dp.length; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (coins[j] <= i) {
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+            return dp[amount] == max ? -1 : dp[amount];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
