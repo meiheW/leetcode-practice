ebe691beebbd42bb43629d937691bcce1d3d0d29
meiheW <1925257530@qq.com>
2022-04-02 17:23:54 +0800

    74. 搜索二维矩阵

---
+++ src/main/java/com/tomster/leetcode/editor/cn/CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,54 @@
+//给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。 请你统计并返回 grid 中 负数 的数目。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+//输出：8
+//解释：矩阵中共有 8 个负数。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [[3,2],[1,0]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 100 
+// -100 <= grid[i][j] <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以设计一个时间复杂度为 O(n + m) 的解决方案吗？ 
+// Related Topics 数组 二分查找 矩阵 
+// 👍 88 👎 0
+
+package com.tomster.leetcode.editor.cn;
+/**
+ * @author: tomster
+ * @data: 2022-04-02 16:39:12
+ */
+public class CountNegativeNumbersInASortedMatrix {
+    public static void main(String[] args) {
+        Solution solution = new CountNegativeNumbersInASortedMatrix().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countNegatives(int[][] grid) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/tomster/leetcode/editor/cn/SearchA2dMatrix.java
+++ src/main/java/com/tomster/leetcode/editor/cn/SearchA2dMatrix.java
@@ -1,40 +1,42 @@
-//编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性： 
+//编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
 //
-// 
-// 每行中的整数从左到右按升序排列。 
-// 每行的第一个整数大于前一行的最后一个整数。 
-// 
 //
-// 
+// 每行中的整数从左到右按升序排列。
+// 每行的第一个整数大于前一行的最后一个整数。
 //
-// 示例 1： 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
 //输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 //输出：true
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 //输出：false
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// m == matrix.length 
-// n == matrix[i].length 
-// 1 <= m, n <= 100 
-// -104 <= matrix[i][j], target <= 104 
-// 
-// Related Topics 数组 二分查找 
+//
+// 提示：
+//
+//
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+//
+// Related Topics 数组 二分查找
 // 👍 298 👎 0
 
 package com.tomster.leetcode.editor.cn;
+
+import java.util.Arrays;
 
 /**
  * @author: tomster
@@ -50,7 +52,30 @@ public class SearchA2dMatrix {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public boolean searchMatrix(int[][] matrix, int target) {
+            int m = matrix.length, n = matrix[0].length;
+            int low = 0, high = m - 1;
+            while (low <= high) {
+                int middle = low + (high - low) / 2;
+                int cur = matrix[middle][0];
+                if (cur > target) {
+                    high = middle - 1;
+                } else if (cur < target) {
+                    low = middle + 1;
+                } else {
+                    return true;
+                }
+            }
+            if (high == -1) {
+                return false;
+            }
+            int[] row = matrix[high];
+            int i = Arrays.binarySearch(row, target);
+            return i >= 0;
+        }
+
+        public boolean searchMatrix1(int[][] matrix, int target) {
             if (matrix == null) {
                 return false;
             }
