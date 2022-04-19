4b3cf105236dbba05301c03716e772a087de7ae4
meiheW <1925257530@qq.com>
2022-04-19 11:39:07 +0800

    278. 第一个错误的版本

---
+++ src/main/java/com/tomster/leetcode/api/VersionControl.java
@@ -0,0 +1,12 @@
+package com.tomster.leetcode.api;
+
+/**
+ * @author meihewang
+ * @date 2022/04/19  11:31
+ */
+public class VersionControl {
+
+    public boolean isBadVersion(int version) {
+        return true;
+    }
+}
